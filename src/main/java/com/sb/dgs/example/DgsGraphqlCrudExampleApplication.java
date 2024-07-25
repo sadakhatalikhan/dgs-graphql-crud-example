4a79a903163752274794b5db47eb1d7a802aaeca
sadakhat <sadakhat11.khan@gmail.com>
2024-07-25 08:13:36 +0530

    initial DGS graphql changes commit

---
+++ src/main/java/com/sb/dgs/example/DgsGraphqlCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sb.dgs.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsGraphqlCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsGraphqlCrudExampleApplication.class, args);
+	}
+
+}
